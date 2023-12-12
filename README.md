8cd8f4c8e65d0aa4809ca7ba5afb8c11f8bfc03f
vinicioslimer <vinicioslimer@outlook.com>
2023-12-11 21:30:37 -0300

    Integrando base do SDK

---
+++ README.md
@@ -0,0 +1,19 @@
+# @sysout/facebook-events
+
+Enable tracking facebook events for ionic capacitor 5
+
+## Install
+
+```bash
+npm install @sysout/facebook-events
+npx cap sync
+```
+
+## API
+
+<docgen-index></docgen-index>
+
+<docgen-api>
+<!-- run docgen to generate docs from the source -->
+<!-- More info: https://github.com/ionic-team/capacitor-docgen -->
+</docgen-api>
---
+++ android/src/main/java/com/sysout/facebookevents/FacebookEventsPlugin.java
@@ -0,0 +1,189 @@
+package com.sysout.facebookEvents;
+
+import com.getcapacitor.Bridge;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.appevents.AppEventsConstants;
+
+import android.os.Bundle;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Iterator;
+
+@CapacitorPlugin(name = "FacebookEvents")
+public class FacebookEventsPlugin extends Plugin {
+    
+    private AppEventsLogger logger;
+
+    @Override
+    public void load() {
+        if (bridge == null) {
+            bridge = this.getBridge();
+        }
+
+        logger = AppEventsLogger.newLogger(bridge.getActivity().getApplicationContext());
+
+        super.load();
+    }
+
+    @PluginMethod
+    public void enableAdvertiserTracking(PluginCall call) {
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void disableAdvertiserTracking(PluginCall call) {
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void getAdvertiserTrackingStatus(PluginCall call) {
+      JSObject returnValue = new JSObject();
+      returnValue.put("status", true);
+      call.resolve(returnValue);
+    }
+
+    @PluginMethod
+    public void logEvent(PluginCall call) {
+        if (!call.getData().has("name")) {
+            call.reject("Must provide an event name");
+            return;
+        }
+
+        String name = call.getString("name");
+        JSObject params = call.getObject("params", new JSObject());
+        Double valueToSum = call.getDouble("valueToSum", null);
+
+        if (params.length() > 0) {
+            Bundle parameters = new Bundle();
+            Iterator<String> iter = params.keys();
+
+            while (iter.hasNext()) {
+                String key = iter.next();
+                String value = params.getString(key);
+                parameters.putString(key, value);
+            }
+            if (valueToSum != null) {
+                logger.logEvent(name, valueToSum, parameters);
+            } else {
+                logger.logEvent(name, parameters);
+            }
+
+        } else {
+            if (valueToSum != null) {
+                logger.logEvent(event, valueToSum);
+            } else {
+                logger.logEvent(event);
+            }
+        }
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void logPurchase(PluginCall call) {
+        if (!call.getData().has("amount")) {
+            call.reject("Must provide an amount");
+            return;
+        }
+
+        Double amount = call.getDouble("amount", null);
+        String curr = call.getString("currency");
+        JSObject params = call.getObject("params", new JSObject());
+
+        Currency currency = Currency.getInstance(curr);
+        if (params.length() > 0) {
+            Bundle parameters = new Bundle();
+            Iterator<String> iter = params.keys();
+
+            while (iter.hasNext()) {
+                String key = iter.next();
+                String value = params.getString(key);
+                parameters.putString(key, value);
+            }
+            logger.logPurchase(BigDecimal.valueOf(amount), currency, parameters);
+        } else {
+            logger.logPurchase(BigDecimal.valueOf(amount), currency);
+        }
+
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void logAddPaymentInfo(PluginCall call) {
+        Integer success = call.getInt("success");
+        Bundle params = new Bundle();
+        params.putInt(AppEventsConstants.EVENT_PARAM_SUCCESS, success);
+        logger.logEvent(AppEventsConstants.EVENT_NAME_ADDED_PAYMENT_INFO, params);
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void logAddToCart(PluginCall call) {
+        if (!call.getData().has("amount")) {
+            call.reject("Must provide an amount");
+            return;
+        }
+        Double amount = call.getDouble("amount", null);
+        String currency = call.getString("currency");
+        Bundle params = new Bundle();
+
+        params.putString(AppEventsConstants.EVENT_PARAM_CURRENCY, currency);
+        logger.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, amount, params);
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void logCompleteRegistration(PluginCall call) {
+        JSObject params = call.getObject("params", new JSObject());
+        if (params.length() > 0) {
+            Bundle parameters = new Bundle();
+            Iterator<String> iter = params.keys();
+
+            while (iter.hasNext()) {
+                String key = iter.next();
+                String value = params.getString(key);
+                parameters.putString(key, value);
+            }
+            logger.logEvent(AppEventsConstants.EVENT_NAME_COMPLETED_REGISTRATION, parameters);
+        } else {
+            logger.logEvent(AppEventsConstants.EVENT_NAME_COMPLETED_REGISTRATION);
+        }
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void logInitiatedCheckout(PluginCall call) {
+        if (!call.getData().has("amount")) {
+            call.reject("Must provide an amount");
+            return;
+        }
+        Double amount = call.getDouble("amount", null);
+
+        JSObject params = call.getObject("params", new JSObject());
+        if (params.length() > 0) {
+            Bundle parameters = new Bundle();
+            Iterator<String> iter = params.keys();
+
+            while (iter.hasNext()) {
+                String key = iter.next();
+                String value = params.getString(key);
+                parameters.putString(key, value);
+            }
+            logger.logEvent(AppEventsConstants.EVENT_NAME_INITIATED_CHECKOUT, amount, parameters);
+        } else {
+            logger.logEvent(AppEventsConstants.EVENT_NAME_INITIATED_CHECKOUT, amount);
+        }
+
+        call.resolve();
+    }
+}
